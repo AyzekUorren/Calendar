a843710aed4973e751199cfa98a0fd547113dac4
PC-ANATOLIY\AyzekUorren <proganatoliy@gmail.com>
2017-06-30 23:43:51 +0300

    Added new methods PrintTableCalendar, Checking the length of the month and the current day

--- src/Code/CalendarHandler.java
+++ src/Code/CalendarHandler.java
@@ -8,6 +8,53 @@ import java.time.temporal.TemporalAdjusters;
 import java.util.Locale;
 
 public class CalendarHandler implements InterfaceForCalendar {
+
+    /*private List<ListOfDays> CalendarList = new ArrayList<ListOfDays>();*/
+
+    private void PrintTableCalendar(int SelectedMonth){
+        String[] ArrayofDaysShort = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        int LastDayOfMonth = GetLengthOfMonth(SelectedMonth);
+        for (String ChoseDay: ArrayofDaysShort) {
+            System.out.format("%9s", ChoseDay);
+        }
+        int CounterofDayNumber = 1;
+        System.out.print("\n\n");
+        if (GetFirstSelectedDayOfMonth(SelectedMonth, 1).getDayOfMonth() != 1)
+        for (int i = 1; i <= 7 - (GetFirstSelectedDayOfMonth(SelectedMonth, 1).getDayOfMonth() - 1); i++){
+            System.out.format("%9s", "");
+            CounterofDayNumber++;
+        }
+        int currentDayNumber = 1;
+        while (currentDayNumber <= LastDayOfMonth){
+            if (CounterofDayNumber == 8){
+                System.out.print("\n\n");
+                CounterofDayNumber = 1;
+            }
+            System.out.format("%7s", "");
+            System.out.format("%02d", currentDayNumber);
+            currentDayNumber++;
+            CounterofDayNumber++;
+        }
+    }
+
+    /*private ListOfDays CreateWeekOfDays(int FirstDay){
+        int CurrentDay = FirstDay;
+        String Days[] = new String[7];
+            for (int IndexOfDay = 0; IndexOfDay < 7; IndexOfDay++) {
+                if (IndexOfDay < FirstDay) {
+                    Days[IndexOfDay] = "1";
+                } else {
+                    Days[IndexOfDay] = String.valueOf(CurrentDay);
+                    CurrentDay++;
+                }
+            }
+            return new ListOfDays(Days[0], Days[1], Days[2], Days[3], Days[4], Days[5], Days[6], CurrentDay);
+    }
+*/
+    /*private int GetNumberDayOfWeek(LocalDate SelectedDate){
+        return SelectedDate.getDayOfWeek().getValue();
+    }*/
+
     private static String GetANSI_StringCodeColour(String chooseсolour) {
         final char EscCode = 27;
         String ANSI_codeColour = null;
@@ -65,8 +112,13 @@ public class CalendarHandler implements InterfaceForCalendar {
         }
         DayOfWeek[] ArrayDaysOfWeek = {null, DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY, DayOfWeek.SATURDAY, DayOfWeek.SUNDAY};
         LocalDate today = LocalDate.now();
-        LocalDate todayInSelectedMonth = LocalDate.of(today.getYear(), SelectedMonth, today.getDayOfMonth());
+        int SelectedDay = today.getDayOfMonth();
 
+        if (GetLengthOfMonth(SelectedMonth) < GetLengthOfMonth(today.getMonthValue())){
+            SelectedDay = 1;
+        }
+
+        LocalDate todayInSelectedMonth = LocalDate.of(today.getYear(), SelectedMonth, SelectedDay);
         return todayInSelectedMonth.with(TemporalAdjusters.firstInMonth(ArrayDaysOfWeek[NumberDayOfWeek]));
     }
 
@@ -81,9 +133,9 @@ public class CalendarHandler implements InterfaceForCalendar {
         if (SelectedMonth > 12 || SelectedMonth < 1) {
             throw new IllegalArgumentException();
         }
-        System.out.println(GetFirstSelectedDayOfMonth(SelectedMonth, 1));
         Month month = Month.of(SelectedMonth);
+        System.out.println("First Monday in selected Month: " + GetFirstSelectedDayOfMonth(SelectedMonth, 1));
         System.out.println(GetANSI_StringCodeColour("Red") + "Month: " + month.getDisplayName(TextStyle.FULL, Locale.forLanguageTag("uk_UA")) + GetANSI_StringCodeColour("Reset"));
-        System.out.println("Month have a " + GetLengthOfMonth(SelectedMonth) + " days");
+        PrintTableCalendar(SelectedMonth);
     }
 }
