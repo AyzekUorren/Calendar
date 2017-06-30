de690b59c8e5b02129f4a9726d18b57016d52a80
PC-ANATOLIY\AyzekUorren <proganatoliy@gmail.com>
2017-06-30 13:19:01 +0300

    Refactor "Main Methods"

--- src/Calendar.java
+++ src/Calendar.java
@@ -2,8 +2,8 @@ import Code.CalendarMethods;
 
 public class Calendar {
     public static void main(String[] ArgsNumber) {
-        CalendarMethods oneMoreCalendarMethods = new CalendarMethods();
-        int NumberOfMonth = Integer.parseInt(ArgsNumber[0]);
-        oneMoreCalendarMethods.DisplayCalendar(NumberOfMonth);
+        CalendarMethods newCalendar = new CalendarMethods();
+
+        newCalendar.DisplayCalendar(newCalendar.GetNumberOfMonth(ArgsNumber));
     }
 }
